65b00ba99402bd9847f0ece114d1f06f85d9d1be
brina-cloud <s.kaliza1@alustudent.com>
2026-02-28 23:51:00 +0200

    add needed fix headers

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -9,8 +9,8 @@ def top_ten(subreddit):
     headers = {"User-Agent": "python:subreddit.top.ten:v1.0"}
     r = requests.get(url, headers=headers, allow_redirects=False)
     if r.status_code != 200:
-        print(None) 
+        print(None)
         return
     info = r.json().get("data", {}).get("children", [])
-    for post in info:
+    for post in info[:10]:
         print(post.get("data", {}).get("title"))
