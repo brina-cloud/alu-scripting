6e943e1cc337882cb910ad500a7f44ca0ee8370c
brina-cloud <s.kaliza1@alustudent.com>
2026-02-28 23:52:56 +0200

    add needed fix headers

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
-""" Module that queries the Reddit API and returns the top ten hot posts"""
+""" 1-top_ten.py """
 import requests
 
 
 def top_ten(subreddit):
-    """ Queries the reddit api and returns the top ten hot posts """
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    headers = {"User-Agent": "python:subreddit.top.ten:v1.0"}
-    r = requests.get(url, headers=headers, allow_redirects=False)
-    if r.status_code != 200:
+    """ prints the titles of the first 10 hot posts listed in a subreddit """
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code != 200:
         print(None)
         return
-    info = r.json().get("data", {}).get("children", [])
-    for post in info[:10]:
-        print(post.get("data", {}).get("title"))
+    posts = response.json()['data']['children']
+    for post in posts:
+        print(post['data']['title'])
