816e3808eb86f8080b3604106cb11bdde0f477de
brina-cloud <s.kaliza1@alustudent.com>
2026-02-28 23:54:38 +0200

    add needed fix headers

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,16 +1,25 @@
 #!/usr/bin/python3
-""" 1-top_ten.py """
-import requests
+"""
+1-top_ten
+"""
+import json
+import urllib.request
 
 
 def top_ten(subreddit):
-    """ prints the titles of the first 10 hot posts listed in a subreddit """
-    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
-    headers = {'User-Agent': 'Mozilla/5.0'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code != 200:
+    """Prints the titles of the first 10 hot posts for a given subreddit"""
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+
+    req = urllib.request.Request(
+        url,
+        headers={"User-Agent": "Mozilla/5.0 (custom bot 0.1)"}
+    )
+
+    try:
+        response = urllib.request.urlopen(req)
+        data = json.loads(response.read().decode("utf-8"))
+        posts = data.get("data", {}).get("children", [])
+        for post in posts:
+            print(post.get("data", {}).get("title"))
+    except Exception:
         print(None)
-        return
-    posts = response.json()['data']['children']
-    for post in posts:
-        print(post['data']['title'])
