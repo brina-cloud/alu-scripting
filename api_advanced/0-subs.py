b36244b919b4028b33f2be2663a6207e418b6d9e
brina-cloud <s.kaliza1@alustudent.com>
2026-02-28 22:39:18 +0200

    fixed module

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 """ Module that queries the Reddit API and returns the number of subscribers"""
 import requests
-
-
 def number_of_subscribers(subreddit):
     """Queries the Reddit API and returns the number of subscribers for a given subreddit."""
 
@@ -11,7 +9,10 @@ def number_of_subscribers(subreddit):
     r = requests.get(url, headers=headers, allow_redirects=False)
     if r.status_code != 200:
     	return 0
-    info = r.json()
-    return (info.get("data", {}).get("subscribers", 0))
+
+    info = r.json().get("data", {})
+    if info is None:
+        return 0
+    return info.get("subscribers", 0)
 
 
