9114f43e1eb7be67a560d33ab294a4d89720db3c
brina-cloud <s.kaliza1@alustudent.com>
2026-02-28 12:12:05 -0800

    added a file to get number of subcribers on subreddit

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-import requests
 """
 module concerning count of subreddit
 """
+
+import requests
 def number_of_subscribers(subreddit):
     url = "https://www.reddit.com/r/{subreddit}/about.json"
     headers = {"User-Agent": "python:subreddit.subscriber.count:v1.0"}
